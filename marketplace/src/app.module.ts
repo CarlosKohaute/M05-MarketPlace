d0abc91bb9d690748e2a013ca57f4afb61605803
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-11-29 05:04:20 -0300

    feat: user entity created

--- marketplace/src/app.module.ts
+++ marketplace/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [UsersModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ marketplace/src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'App running! 🚀 Visit http://localhost:3334/docs to see documentation.';
+  }
+}
