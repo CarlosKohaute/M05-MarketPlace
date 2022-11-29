4b441b45e7d2fa7cd5170979a2255dfdb352bcea
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-11-29 19:30:56 -0300

    Feat: create user status on controller

--- marketplace/src/users/users.controller.ts
+++ marketplace/src/users/users.controller.ts
@@ -8,27 +8,38 @@ import {
   Delete,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
 @ApiTags('user')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  @ApiOperation({
+    summary: 'Criar novo usuário.',
+  })
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
+  @ApiOperation({
+    summary: 'Listar todos os usuários.',
+  })
+  findAll(): Promise<User[]> {
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  @ApiOperation({
+    summary: 'Listar usuário por id.',
+  })
+  findOne(@Param('id') id: string): Promise<User> {
+    return this.usersService.findOne(id);
   }
 
   @Patch(':id')
