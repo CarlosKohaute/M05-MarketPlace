dd6dc2e242c86361dfcffd9d329af73b828d19a8
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-11-29 20:13:51 -0300

    feat: create error treatments

--- marketplace/src/users/users.service.ts
+++ marketplace/src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -14,18 +18,30 @@ export class UsersService {
   create(dto: CreateUserDto): Promise<User> {
     const data: User = { ...dto };
 
-    return this.prisma.user.create({ data });
+    return this.prisma.user.create({ data }).catch(this.handleError);
   }
-
-  findOne(id: string): Promise<User> {
-    return this.prisma.user.findUnique({
+  handleError(error: Error) {
+    console.log(error.message);
+    throw new UnprocessableEntityException(error.message);
+    return undefined;
+  }
+  async findById(id: string): Promise<User> {
+    const record = await this.prisma.user.findUnique({
       where: {
         id,
       },
     });
+    if (!record) {
+      throw new NotFoundException(`User com o ${id} não encontrado`);
+    }
+    return record;
   }
 
-  update(id: string, dto: UpdateUserDto): Promise<User> {
+  async findOne(id: string): Promise<User> {
+    return this.findById(id);
+  }
+  async update(id: string, dto: UpdateUserDto): Promise<User> {
+    await this.findById(id);
     const data: Partial<User> = { ...dto };
 
     return this.prisma.user.update({
@@ -35,6 +51,8 @@ export class UsersService {
   }
 
   async delete(id: string) {
+    await this.findById(id);
+
     await this.prisma.user.delete({ where: { id } });
   }
 }
