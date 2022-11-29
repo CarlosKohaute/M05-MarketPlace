cbbe6de8ba0334740aac9a505c27f86f2ea405d5
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-11-29 06:25:32 -0300

    Feat: created prisma module and prisma service

--- marketplace/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'App running! 🚀 Visit http://localhost:3334/docs to see documentation.';
-  }
-}
