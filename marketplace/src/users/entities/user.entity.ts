0b3e147a0695341da120a8f584f6d9fd20c44d48
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-12-11 15:33:01 -0300

    feat: refactored user route

--- marketplace/src/users/entities/user.entity.ts
+++ marketplace/src/users/entities/user.entity.ts
@@ -2,8 +2,9 @@ export class User {
   id?: string;
   name: string;
   email: string;
-  password: string;
+  password?: string;
   imageURL: string;
-  createdAt?: Date;
-  updatedAt?: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  favorites?: Favorite[];
 }
